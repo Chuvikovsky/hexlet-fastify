aadea31d8e0d170dad754720919ad377d89a911c
Chuvikovsky <dchuvikovsky@mail.ru>
2024-10-25 17:38:44 +0300

    add /hello page with queries

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ const app = fastify();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.type('text/html');
+    res.send('<h1>Hello Hexlet</h1>')
+});
+
+app.get('/users', (req, res) => {
+    res.send(req.originalUrl);
+});
+
+app.post('/users', (req, res) => {
+    res.send('POST /users');
+});
+
+app.get('/hello', (req, res)=>{
+    const {name = 'World'} = req.query;
+    res.send(`Hello, ${name}!`);
 });
 
 app.listen({port}, () => {
