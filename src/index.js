34dd2c089d617b2a06e6a046d10922643eb1a244
Chuvikovsky <dchuvikovsky@mail.ru>
2024-10-28 11:15:18 +0300

    add http session lesson

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+
+import users from './users.js';
 
 const app = fastify();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.type('text/html');
-    res.send('<h1>Hello Hexlet</h1>')
+  res.type('text/html');
+  res.send('<h1>Hello Hexlet</h1>');
+});
+
+app.get('/hello', (req, res) => {
+  const { name = 'World' } = req.query;
+  res.send(`Hello, ${name}!`);
 });
 
 app.get('/users', (req, res) => {
-    res.send(req.originalUrl);
+  let { page = 1, per = 5 } = req.query;
+  page = parseInt(page, 10);
+  per = parseInt(per, 10);
+  const start = page * per - per;
+  const stop = page * per;
+  const selectedUsers = users.slice(start, stop);
+  res.send(selectedUsers);
 });
 
-app.post('/users', (req, res) => {
-    res.send('POST /users');
-});
-
-app.get('/hello', (req, res)=>{
-    const {name = 'World'} = req.query;
-    res.send(`Hello, ${name}!`);
-});
-
-app.listen({port}, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen({ port }, () => {
+  console.log(`Example app listening on port ${port}`);
 });
